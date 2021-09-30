d2d987ea08227b6f9129094fd44bc2129200f90e
Roseoketch <oketchrose@gmail.com>
2021-09-30 21:18:32 +0300

    adding counter section

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 2000
+  });
+  $('.counter').addClass('animated fadeInDownBig');
+  $('h3').addClass('animated fadeIn');
